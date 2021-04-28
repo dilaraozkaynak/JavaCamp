dc16f1425651d7de59d9996de51cc15b41a24ed7
Dilara <dilaraaozk@gmail.com>
2021-04-29 00:34:07 +0300

    Java 3. Gün değişiklikler eklendi.

--- oopIntro/src/oopIntro/ProductManager.java
+++ oopIntro/src/oopIntro/ProductManager.java
@@ -2,6 +2,6 @@ package oopIntro;
 
 public class ProductManager {
 	public void addToCart(Product product) {
-		System.out.println("Sepete eklendi " + product.name.toUpperCase());
+		System.out.println("Sepete eklendi " + product.getName());
 	}
 }
