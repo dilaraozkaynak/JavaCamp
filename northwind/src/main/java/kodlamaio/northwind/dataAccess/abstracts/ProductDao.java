66f0faf47a0d7b893a53250242812b9fc23c1bff
Dilara <dilaraaozk@gmail.com>
2021-06-02 20:46:30 +0300

    9. Gün Ders Sonu

--- northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
+++ northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -6,22 +6,28 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import kodlamaio.northwind.entities.concretes.Product;
+import kodlamaio.northwind.entities.dtos.ProductWithCategoryDto;
 
-public interface ProductDao extends JpaRepository<Product, Integer>{
+public interface ProductDao extends JpaRepository<Product, Integer> {
 	Product getByProductName(String productName);
-	
+
 	Product getByProductNameAndCategory_CategoryId(String productName, int categoryId);
-	
+
 	List<Product> getByProductNameOrCategory_CategoryId(String productName, int categoryId);
-	
+
 	List<Product> getByCategoryIn(List<Integer> categories);
-	
+
 	List<Product> getByProductNameContains(String productName);
-	
+
 	List<Product> getByProductNameStartsWith(String productName);
-	
+
 	@Query("From Product where productName=:productName and category.categoryId=:categoryId")
-	List<Product> getByNameAndCategory(String productName, int categoryId);		
-	
-	//select * from products where product_name=bisey and categoryId=bisey
+	List<Product> getByNameAndCategory(String productName, int categoryId);
+
+	@Query("Select new kodlamaio.northwind.entities.dtos.ProductWithCategoryDto"
+			+ "(p.id, p.productName, c.categoryName)" + "From Category c Inner Join c.products p")
+	List<ProductWithCategoryDto> getProductWithCategoryDetails();
+
+	// select p.product_id, p.product_name, c.category_name from categories c inner
+	// join products p on category_id=p.category_id
 }
